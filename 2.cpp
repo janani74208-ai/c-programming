55848509bf567d14998345647dbb4a948b894d86
janani74208-ai <pushpapushpa74208@gmail.com>
2025-12-06 09:39:52 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()	
+{ 
+	int r;
+	float ac,acc;
+	printf("enter r value");
+	scanf("%d",&r);
+	ac=3.1415*r*r;
+	acc=2*3.1415*r;
+    printf("Area of circle is:%f\n",ac);
+    printf("Area of circuference is:%f",acc);
+    return 0;
+}
+
+
